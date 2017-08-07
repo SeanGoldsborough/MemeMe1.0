2a88c18ed2e2c2657c8d8175fee5c0423ce1d384
Sean Goldsborough <smgoldsborough@seangolmacbook3.cable.rcn.com>
2017-08-06 20:25:47 -0400

    Initial Commit

---
+++ MemeMe1.0Attempt1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe1.0Attempt1
+//
+//  Created by Sean Goldsborough on 8/6/17.
+//  Copyright © 2017 Sean Goldsborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
